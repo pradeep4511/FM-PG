ac343a0b3b65944b23cd65cd91566658cfee904b
pradeep <147896275+pradeep4511@users.noreply.github.com>
2026-09-07 07:34:32 +0000

    Configure PWA service worker

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "node:path";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: false,
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
+      },
+    }),
+  ],
+
   build: {
     rollupOptions: {
       input: {
